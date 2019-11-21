30098485930ede639a41e2df653ffaa966fe4d13
PascalDragos <dcp_eu@yahoo.com>
2019-11-21 18:56:21 +0200

    TX and RX basic

--- RXmain.py
+++ RXmain.py
@@ -2,14 +2,14 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # ipv4, UDP
 address_port = ("127.0.0.1", 5005)
-buffer_size = 1024
+buffer_size = 1024  # pachetul va contine 512B
 
 sock.bind(address_port)
 
 
 while True:
     data, addr = sock.recvfrom(buffer_size)
-    print(data)
-    print(addr)
+    print(data.decode("UTF-8") + " received from" + str(addr))
+
 
 
--- TXmain.py
+++ TXmain.py
@@ -1,8 +1,8 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # ipv4, UDP
-address_port = ("127.0.0.1", 5002)
+address_port = ("127.0.0.1", 5005)
 
 
-sock.sendto("Mesaj trimis", address_port)
+sock.sendto(("Mesaj trimis").encode("UTF-8"), address_port)
 
