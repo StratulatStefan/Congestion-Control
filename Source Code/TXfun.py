639df0753c9b0aca3c2082210ec81fb01d68755b
PascalDragos <dcp_eu@yahoo.com>
2019-11-22 09:44:16 +0200

    Proiect saptamana 8

--- Source Code/TXfun.py
+++ Source Code/TXfun.py
@@ -1,27 +1,50 @@
-size = 512
+DEFAULT_SIZE = 1
+ack = 11
+
+
+# a = int.from_bytes(segment_number, byteorder='big', signed=False)
+# print(a)
+
+def incrementAck():
+    global ack
+    ack = ack + 1
 
 #impachetarea primului segment care va contine numele si extensia
-def encode_start(data):
-#    ack = b'\x00\x00\x00\x00'
-#   startCode = b'\x00\x01'
-    ack = 0x00_00_00_00
-    segment_code = 0x_00_00_00_01
-    segment = bytes(ack, segment_code)
+def encode_start(segment_data):
+    incrementAck()
+    segment_number = ack.to_bytes(4, byteorder='big', signed=False)
+    segment_type = b'\x01'
+    segment_data_len = len(segment_data)
+    segment_len = segment_data_len.to_bytes(2, byteorder='big', signed=False)
+    segment = segment_number + segment_type + segment_len + segment_data  # impachetarea efectiva
     return segment
 
+
 #impachetarea unui segment din interiorul fisierului
-def encode_data(data):
-    pass
+def encode_data(segment_data):
+    incrementAck()
+    segment_number = ack.to_bytes(4, byteorder='big', signed=False)
+    segment_type = b'\x02'
+    segment_data_len = DEFAULT_SIZE
+    segment_len = segment_data_len.to_bytes(2, byteorder='big', signed=False)
+    segment = segment_number + segment_type + segment_len + segment_data
+    return segment
 
 
 #impachetarea ulitmului segment
 #in campul de segment_code, al doilea octet va fi lungimea caracterelor utile
-def encode_end(data):
+def encode_end(segment_data):
+    incrementAck()
+    segment_number = ack.to_bytes(4, byteorder='big', signed=False)
+    segment_type = b'\x03'
+    segment_data_len = len(segment_data)
+    segment_data += segment_data * (DEFAULT_SIZE - segment_data_len)
+    segment_len = segment_data_len.to_bytes(2, byteorder='big', signed=False)
+    segment = segment_number + segment_type + segment_len + segment_data
+    return segment
 
-    pass
 
-
-def encode_error(data):
+def encode_error(segment_data):
     pass
 
 
@@ -37,7 +60,7 @@ def encode(tip, data):
 
 
 #citirea fisier ca pachete de octeti
-def bytes_from_file(filename, chunk_size=(size - 6)):
+def bytes_from_file(filename, chunk_size=DEFAULT_SIZE):
     with open(filename, "rb") as f:
         while True:
             chunk = f.read(chunk_size)
@@ -50,7 +73,8 @@ def bytes_from_file(filename, chunk_size=(size - 6)):
 #codificarea pachetelor de octeti
 def encode_bytes(filename):
     for b in bytes_from_file(filename):
-        if len(b) == (size - 6):
+        if len(b) == DEFAULT_SIZE:
             yield encode('DATA', b)
         else:
-            yield encode('END', b)
+            #yield encode('END', b)
+            yield encode('DATA', b)
--- Source Code/TXmain.py
+++ Source Code/TXmain.py
@@ -6,11 +6,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # ipv4, UDP
 address_port = ("127.0.0.1", 5005)
 file_name_to_send = "fisier.txt"
 
+
+encode_start(b'\x14\xff')
+
+'''
 segment = encode('START', file_name_to_send)
 sock.sendto(segment, address_port)
+'''
+
+#for segment in bytes_from_file(file_name_to_send):
+#    print(segment)
+
 
 for segment in encode_bytes(file_name_to_send):
-    sock.sendto(segment, address_port)
+    pass
+    #print(segment)
+    #sock.sendto(segment, address_port)
 
 # sock.sendto(("Mesaj trimis").encode("UTF-8"), address_port)
 
