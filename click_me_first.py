e95220eec67cec747473809235a0a151580100ac
Stratulat Stefan <56991532+StratulatStefan@users.noreply.github.com>
2020-01-15 05:37:09 -0800

    click it to install pyside2

---
+++ click_me_first.py
@@ -0,0 +1,14 @@
+'''
+hey, I will install Pyside2 for you
+Pyside2 is the library needed for the UI.
+'''
+
+
+import os
+os.system('python -m pip install pyside2')
+
+
+'''
+that's all
+not a big deal
+'''
